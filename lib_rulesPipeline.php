797c753a2e32743edbe1d15cb6d670d4d7e80b80
Reviakin@home <reviakin@gmail.com>
2024-11-14 21:46:51 +0500

    infrastructure and service supports merged

--- lib_rulesPipeline.php
+++ lib_rulesPipeline.php
@@ -364,14 +364,14 @@ class rulesPipeline {
 	}
 
 	public static function macroInventoryServiceman($iHost) {
-		return inventoryApi::fetchUserNames([$iHost['responsible'],$iHost['serviceResponsible']]);
+		return inventoryApi::fetchUserNames([$iHost['responsible'],$iHost['servicesResponsible']]);
 	}
 
 	public static function macroInventorySupportTeam($iHost) {
 		//print_r($iHost['supportTeam']);
 		return inventoryApi::fetchUserNames(
-		    array_merge($iHost['supportTeam'],$iHost['serviceSupportTeam']),
-            [$iHost['responsible'],$iHost['serviceResponsible']]
+		    array_merge($iHost['supportTeam'],$iHost['servicesSupportTeam']),
+            [$iHost['responsible'],$iHost['servicesResponsible']]
         );
 	}
 
