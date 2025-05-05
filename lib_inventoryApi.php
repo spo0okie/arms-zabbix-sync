9bc4e2479760c5d96e37a79287ba646f6b48df5f
Reviakin@home <reviakin@gmail.com>
2025-05-05 11:27:19 +0500

    techs fqdn load

--- lib_inventoryApi.php
+++ lib_inventoryApi.php
@@ -79,7 +79,7 @@ class inventoryApi {
 	 */
 	public function cacheTechs() {
 
-		$data=$this->req('/api/techs/?showArchived=1&per-page=0&expand=responsible,comp,site,supportTeam,stateName,type,model,manufacturer,services');
+		$data=$this->req('/api/techs/?showArchived=1&per-page=0&expand=responsible,comp,site,supportTeam,stateName,type,model,manufacturer,services,fqdn');
 		$obj=json_decode($data,true);
 		foreach ($obj as $tech) {
 			$tech['class']='techs';
